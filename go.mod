3b6b32de13d115338d36248de766360300207c70
Chris Trott <908409+trotttrotttrott@users.noreply.github.com>
2020-05-09 12:23:49 -0600

    Load config from farto.yaml

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/aws/aws-sdk-go v1.30.24
 	github.com/go-clix/cli v0.1.1
+	gopkg.in/yaml.v2 v2.2.2
 )
--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+
 	"github.com/go-clix/cli"
+	"gopkg.in/yaml.v2"
 )
+
+type config struct {
+	S3Region string `yaml:"s3Region"`
+	S3Bucket string `yaml:"s3Bucket"`
+	S3Prefix string `yaml:"s3Prefix"`
+}
 
 func main() {
 	rootCmd := &cli.Command{
@@ -23,23 +32,33 @@ func main() {
 	}
 }
 
+func getConfig() (c config, err error) {
+	y, err := ioutil.ReadFile("farto.yaml")
+	if err != nil {
+		return
+	}
+	err = yaml.Unmarshal(y, &c)
+	return
+}
+
 func generateCmd() *cli.Command {
 	cmd := &cli.Command{
 		Use:   "generate",
 		Short: "Generate static site locally.",
 	}
 	cmd.Run = func(cmd *cli.Command, args []string) error {
-
+		c, err := getConfig()
+		if err != nil {
+			return err
+		}
 		sess, err := session.NewSession(&aws.Config{
-			Region: aws.String("us-west-2")},
+			Region: aws.String(c.S3Region)},
 		)
 		if err != nil {
 			return err
 		}
-
 		svc := s3.New(sess)
-
-		b, err := walkBucket(svc, "farto.cloud")
+		b, err := walkBucket(svc, c.S3Bucket, c.S3Prefix)
 		if err != nil {
 			return err
 		}
