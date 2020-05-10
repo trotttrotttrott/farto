bab0c0792f7b9ffe253bc4bdc3c9d77b89e4282b
Chris Trott <908409+trotttrotttrott@users.noreply.github.com>
2020-05-09 22:55:35 -0600

    Don't cache site/*

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,6 +34,12 @@ resource "dnsimple_record" "test" {
 
 resource "aws_cloudfront_distribution" "farto_cloud" {
 
+  aliases             = local.aliases
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "site/index.html"
+  price_class         = "PriceClass_All"
+
   origin {
     domain_name = aws_s3_bucket.farto_cloud.bucket_domain_name
 
@@ -51,29 +57,38 @@ resource "aws_cloudfront_distribution" "farto_cloud" {
     }
   }
 
-  aliases             = local.aliases
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "site/index.html"
-  price_class         = "PriceClass_All"
+  viewer_certificate {
+    acm_certificate_arn            = "arn:aws:acm:us-east-1:081549132651:certificate/cdb6e100-6cd2-41f8-9bec-56ac0fd03293"
+    cloudfront_default_certificate = false
+    minimum_protocol_version       = "TLSv1.2_2018"
+    ssl_support_method             = "sni-only"
+  }
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.origin_id
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = local.origin_id
+    viewer_protocol_policy = "redirect-to-https"
     forwarded_values {
       query_string = false
       cookies {
         forward = "none"
       }
     }
-    viewer_protocol_policy = "redirect-to-https"
   }
 
-  viewer_certificate {
-    acm_certificate_arn            = "arn:aws:acm:us-east-1:081549132651:certificate/cdb6e100-6cd2-41f8-9bec-56ac0fd03293"
-    cloudfront_default_certificate = false
-    minimum_protocol_version       = "TLSv1.2_2018"
-    ssl_support_method             = "sni-only"
+  ordered_cache_behavior {
+    path_pattern           = "/site/*"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = local.origin_id
+    default_ttl            = 0
+    viewer_protocol_policy = "redirect-to-https"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
   }
 }
