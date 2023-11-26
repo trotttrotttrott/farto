c6bee86f976357e94fc8af7ccef8557a3c8d8aaf
Chris Trott <908409+trotttrotttrott@users.noreply.github.com>
2023-11-26 12:45:36 -0500

    s3 tf module migrated off deprecated fields

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,5 +1,30 @@
 resource "aws_s3_bucket" "farto_cloud" {
   bucket = var.bucket
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "farto_cloud" {
+  bucket = aws_s3_bucket.farto_cloud.id
+
+  rule {
+    bucket_key_enabled = false
+
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "farto_cloud" {
+  bucket = aws_s3_bucket.farto_cloud.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "farto_cloud" {
+  bucket = aws_s3_bucket.farto_cloud.id
   acl    = "private"
 }
 
